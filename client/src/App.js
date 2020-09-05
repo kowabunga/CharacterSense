98bf77229d78f2457300d88f4fb7a00bd6a54ec7
Anthony Siletti <anthony.siletti@gmail.com>
2020-09-05 13:18:45 -0400

    working

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import WoWState from './context/wow/WowState';
-import Header from './components/layout/Header';
-import HomePage from './components/pages/HomePage';
-import Dashboard from './components/pages/Dashboard';
 import './App.css';
 
 function App() {
   return (
     <WoWState>
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Switch>
           <Route exact path='/'>
-            <HomePage />
+            {/* <HomePage /> */}
           </Route>
           <Route exact path='/dashboard/:token'>
-            <Dashboard />
+            {/* <Dashboard /> */}
           </Route>
         </Switch>
       </Router>
